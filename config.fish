0c521e9322920a643547ba4913fca0a73ee2041b
Jonathan Feinberg <jonathf@gmail.com>
2020-04-20 13:18:00 +0200

    cleanup small stuff

--- config.fish
+++ config.fish
@@ -5,20 +5,13 @@ set --export EDITOR nvim
 set --export VISUAL nvim
 
 # Where are we? Used in some of the scripts.
-set --universal FISH_ROOT (dirname (readlink -f (status -f))
-)
+set --universal FISH_ROOT (dirname (readlink -f (status -f)))
 
 # If a local.fish file exist in your fish folder, source it.
 # For storing local system stuff and secrets.
 if test -f $FISH_ROOT/local.fish
     source $FISH_ROOT/local.fish
 end
-
-if test -f ~/.matplotlibrc
-    set --universal MATPLOTLIBRC ~/.matplotlibrc
-end
-
-set --export DIRENV_LOG_FORMAT
 
 if test -d $HOME/.cargo
     set --export CARGO_ROOT "$HOME/.cargo"
@@ -28,6 +21,8 @@ end
 if test -d $HOME/.pyenv
     set --export PYENV_ROOT "$HOME/.pyenv"
     set --export --append PATH "$PYENV_ROOT/bin"
+else
+    echo "pyenv is missing; install by running: clone pyenv/pyenv ~/.pyenv"
 end
 
 set direnv_path (command -v direnv)
@@ -36,6 +31,8 @@ if test -n "$direnv_path"
     if not contains "$PYENV_ROOT/shims" $PATH
         pyenv init - | source
     end
+else
+    echo "direnv is missing; install by running: aurbuild direnv"
 end
 
 # Deactivate the default virtualenv prompt so that we can add our own
@@ -44,3 +41,5 @@ set --global --export VIRTUAL_ENV_DISABLE_PROMPT 1
 set --universal PIPENV_VENV_IN_PROJECT true
 set --universal PIPENV_DEFAULT_PYTHON_VERSION 3.7
 set --universal PIPENV_IGNORE_VIRTUALENVS 1
+
+set fish_color_autosuggestion brblack
--- functions/aurbuild.fish
+++ functions/aurbuild.fish
@@ -1,11 +1,17 @@
 function aurbuild --description 'download and install AUR' --wraps 'makepkg'
-	set folder (string split "/" "$argv[1]")[-1]
+	set --local uri $argv[1]
+	set --local folder (string split "/" "$uri")[-1]
+
+	if not string match -r '/' $uri >/dev/null
+		set uri "https://aur.archlinux.org/$uri.git"
+	end
+
 	set folder "/home/$USER/src/$folder"
 	if test -d $folder
 		echo "folder $folder already exist; aborting"
 		return 1
 	end
-	clone $argv[1] $folder
+	clone $uri $folder
 	pushd $folder
 	makepkg --syncdeps --install $argv[2..-1]
 	popd
