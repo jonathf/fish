27e7878a8c8ad4cbf3c49c1d6966a147ee469c98
Jonathan Feinberg <jonathf@gmail.com>
2020-05-09 14:13:17 +0200

    pretty greeting and colors

--- config.fish
+++ config.fish
@@ -46,4 +46,28 @@ set --universal PIPENV_VENV_IN_PROJECT true
 set --universal PIPENV_DEFAULT_PYTHON_VERSION 3.7
 set --universal PIPENV_IGNORE_VIRTUALENVS 1
 
-set fish_color_autosuggestion brblack
+# Fish color scheme match terminal
+set -U fish_color_autosuggestion      brblack
+set -U fish_color_command             brblue
+set -U fish_color_comment             brred
+set -U fish_color_cwd_root            red
+set -U fish_color_end                 brmagenta
+set -U fish_color_error               brred
+set -U fish_color_escape              brcyan
+set -U fish_color_history_current     --bold
+set -U fish_color_host                normal
+set -U fish_color_match               --background=brblue
+set -U fish_color_normal              normal
+set -U fish_color_operator            cyan
+set -U fish_color_param               brblue
+set -U fish_color_quote               yellow
+set -U fish_color_redirection         bryellow
+set -U fish_color_search_match        'bryellow' '--background=brblack'
+set -U fish_color_selection           'white' '--bold' '--background=brblack'
+set -U fish_color_status              red
+set -U fish_color_user                brgreen
+set -U fish_color_valid_path          --underline
+set -U fish_pager_color_completion    normal
+set -U fish_pager_color_description   yellow
+set -U fish_pager_color_prefix        'white' '--bold' '--underline'
+set -U fish_pager_color_progress      'brwhite' '--background=cyan'
--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,2 +1,12 @@
 function fish_greeting
+    set --local colors R G Y B M C K
+    set --local codes 1m 2m 3m 4m 5m 6m 7m
+    for idx in (seq 1 7)
+        echo -en  "\e[0;1;3$codes[$idx] $colors[$idx] \e[1;30;4$codes[$idx] $idx "
+    end
+    echo -e "\033[m "
+    for idx in (seq 1 7)
+        echo -en  "\e[1;30;10$codes[$idx] $colors[$idx] \e[0;1;3$codes[$idx] $idx "
+    end
+    echo -e "\033[m "
 end
